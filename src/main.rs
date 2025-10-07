ab96e81d7304145f0d495404c562682725118b32
augustuswm <gusmayo@gmail.com>
2025-10-07 09:39:41 -0500

    Work towards release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::net::{SocketAddr, SocketAddrV4};
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::net::{SocketAddr, SocketAddrV4};
 use tap::TapFallible;
 use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::EnvFilter;
